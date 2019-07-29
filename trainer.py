9f0846c89a0a8a040c1abc25ff55ce17223af835
Tobias Fischer <tobias.fischer@posteo.net>
2019-07-29 22:36:25 +0200

    keyboard preparations

--- trainer.py
+++ trainer.py
@@ -6,12 +6,40 @@ import random
 
 from time import time
 
+US_MAJOR = \
+    """                     ~ ! @ # $ % ^ & * ( ) _ +
+                        Q W E R T Y U I O P { } |
+                         A S D F G H J K L : "
+                          Z X C V B N M < > ?"""
+
+US_MINOR = \
+    """                     ` 1 2 3 4 5 6 7 8 9 0 - =
+                        q w e r t y u i o p [ ] \\
+                         a s d f g h j k l ; '
+                          z x c v b n m , . /"""
+
+fingers = {
+    'L_pinky': '`12qaz~!@QAZ',
+    'L_ring': '3wsx#WSX',
+    'L_middle': '4edc$EDC',
+    'L_index': '5rfv6tgb%RFV^TGB',
+
+    'R_index': '7yhn8ujm&YHN*UJM',
+    'R_middle': '9ik,(IK<',
+    'R_ring': '0ol.)OL>',
+    'R_pinky': """-p;/=[']\\_P:?+{"}|""",
+
+    'thumb': ' '
+}
+
+
+
 numbers = '1234567890'
 small = 'abcdefghijklmnopqrstuvwxyz'
 big = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 brackets = '[]{}()<>'
 punctuation = '`.:,;!?'
-slashes_and_dashes = '/\|'
+slashes_and_dashes = '/\\|'
 upper_row = '~!@#$%^&*()_+-='
 quotes = ''''"'''
 
@@ -21,7 +49,6 @@ lengths = [random.randint(2, 8) for _ in range(100)]
 words = [''.join(random.choices(symbols, k=length)) for length in lengths]
 text = ' '.join(words)
 typed = []
-timing = []
 
 
 def draw(screen, index, letters_per_minute, error_rate):
@@ -40,20 +67,28 @@ def draw(screen, index, letters_per_minute, error_rate):
     screen.addstr(0, 0, ' ' * left_pad + text_cutout)
 
 
+def draw_keyboard(screen):
+    screen.addstr(3, 0, US_MINOR)
+
+
 def main(stdscr):
     curses.curs_set(False)
     curses.use_default_colors()
     key = None
     index = 0
     errors = 0
+    start_time = 0
     while True:
-        error_rate = errors / index * 1000 if index > 0 else 0
-        draw(stdscr, index, 'Hi Judith ▼', int(error_rate))
-        # display_key(key, stdscr)
-        key = get_key(stdscr)
 
+        error_rate = errors / index * 1000 if index > 0 else 0
+        letters_per_minute = 60 * index / (time() - start_time)
+        draw(stdscr, index, letters_per_minute, int(error_rate))
+
+        draw_keyboard(stdscr)
+
+        key = get_key(stdscr)
         if not index:
-            start = time()
+            start_time = time()
 
         if key == text[index]:
             index += 1
