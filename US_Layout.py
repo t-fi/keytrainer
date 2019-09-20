f81d29f97504c896b625e36cb55dfd01b485fc0e
Tobias Fischer <tobias.fischer@posteo.net>
2019-09-20 17:24:19 +0200

    major refactoring, refactoring out classes for keyboard and textpad

---
+++ US_Layout.py
@@ -0,0 +1,51 @@
+import random
+
+
+def random_words(length_range, count):
+    symbols = minor_keys + major_keys
+    lengths = [random.randint(*length_range) for _ in range(count)]
+    words = [''.join(random.choices(symbols, k=length)) for length in lengths]
+    text = ''.join(words)
+    return text
+
+
+start_ends = [[0, 25], [3, 28], [4, 25], [5, 24]]
+key_coords = []
+for row, start_end in enumerate(start_ends):
+    for i in range(*start_end, 2):
+        key_coords.append([row, i])
+
+major_keys = '~!@#$%^&*()_+QWERTYUIOP{}|ASDFGHJKL:"ZXCVBNM<>?'
+minor_keys = "`1234567890-=qwertyuiop[]\\asdfghjkl;'zxcvbnm,./"
+
+finger_keys = {
+    'L_pinky': '`12qaz~!@QAZ',
+    'L_ring': '3wsx#WSX',
+    'L_middle': '4edc$EDC',
+    'L_index': '5rfv6tgb%RFV^TGB',
+
+    'R_index': '7yhn8ujm&YHN*UJM',
+    'R_middle': '9ik,(IK<',
+    'R_ring': '0ol.)OL>',
+    'R_pinky': """-p;/=[']\\_P:?+{"}|""",
+
+    'thumb': ' '
+}
+
+key_fingers = {key: finger for finger, keys in finger_keys.items() for key in keys}
+
+finger_colors = {
+    'L_pinky': 28,
+    'L_ring': 2,
+    'L_middle': 42,
+    'L_index': 0,
+
+    'R_index': 34,
+    'R_middle': 164,
+    'R_ring': 78,
+    'R_pinky': 221,
+
+    'thumb': 0
+}
+
+key_colors = {key: finger_colors[key_fingers[key]] for key in major_keys + minor_keys}
--- trainer.py
+++ trainer.py
@@ -2,97 +2,28 @@
 
 import curses
 from curses import wrapper
-import random
+import US_Layout
+import rendering
 
 from time import time
 
-US_MAJOR = \
-    """                     ~ ! @ # $ % ^ & * ( ) _ +
-                        Q W E R T Y U I O P { } |
-                         A S D F G H J K L : "
-                          Z X C V B N M < > ?"""
-
-US_MINOR = \
-    """                     ` 1 2 3 4 5 6 7 8 9 0 - =
-                        q w e r t y u i o p [ ] \\
-                         a s d f g h j k l ; '
-                          z x c v b n m , . /"""
-
-fingers = {
-    'L_pinky': '`12qaz~!@QAZ',
-    'L_ring': '3wsx#WSX',
-    'L_middle': '4edc$EDC',
-    'L_index': '5rfv6tgb%RFV^TGB',
-
-    'R_index': '7yhn8ujm&YHN*UJM',
-    'R_middle': '9ik,(IK<',
-    'R_ring': '0ol.)OL>',
-    'R_pinky': """-p;/=[']\\_P:?+{"}|""",
-
-    'thumb': ' '
-}
-
-numbers = '1234567890'
-small = 'abcdefghijklmnopqrstuvwxyz'
-big = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-brackets = '[]{}()<>'
-punctuation = '`.:,;!?'
-slashes_and_dashes = '/\\|'
-upper_row = '~!@#$%^&*()_+-='
-quotes = ''''"'''
-
-symbols = numbers + small + big + brackets + punctuation + slashes_and_dashes + upper_row + quotes
-
-lengths = [random.randint(2, 8) for _ in range(100)]
-words = [''.join(random.choices(symbols, k=length)) for length in lengths]
-text = ' '.join(words)
-typed = []
-
-
-def draw(screen, index, letters_per_minute, error_rate):
-    h, w = screen.getmaxyx()
-    if h < 3 or w < 75:
-        raise RuntimeError(f"window is too small! Required: 5x75, got {h}x{w}")
-
-    screen.clear()
-    screen.addstr(2, 4, f"Keys per minute: {letters_per_minute}")
-    screen.addstr(2, 46, f"Error rate: {error_rate}‰")
-    screen.addstr(1, 36, "Λ")
-    screen.addstr(2, 36, "|")
-
-    left_pad = 36 - index
-    text_cutout = text[max(index - 36, 0):index + 36]
-    screen.addstr(0, 0, ' ' * left_pad + text_cutout)
-
-
-def draw_keyboard(screen):
-    screen.addstr(3, 0, US_MINOR)
-
 
 def main(stdscr):
-    curses.curs_set(False)
-    curses.use_default_colors()
-    key = None
-    index = 0
-    errors = 0
-    start_time = 0
+    rendering.setup_curses_colors()
+
+    text = US_Layout.random_words([4, 8], 10)
+
+    keyboard = rendering.KeyboardPadRenderer([3, 5], US_Layout)
+    text_pad = rendering.TextPad([1, 18], 10, text)
+    stdscr.refresh()
+    keyboard.draw()
+    text_pad.draw()
+    stdscr.refresh()
     while True:
-
-        error_rate = errors / index * 1000 if index > 0 else 0
-        letters_per_minute = 60 * index / (time() - start_time)
-        draw(stdscr, index, letters_per_minute, int(error_rate))
-
-        draw_keyboard(stdscr)
-
         key = get_key(stdscr)
-        if not index:
-            start_time = time()
-
-        if key == text[index]:
-            index += 1
-        else:
-            errors += 1
-
+        text_pad.trigger_next(key)
+        keyboard.draw_highlight_key(text_pad.current_char)
+        text_pad.draw()
         stdscr.refresh()
 
 
@@ -102,15 +33,8 @@ def get_key(stdscr):
     except Exception as e:
         print('Cannot get key')
         print(e)
+        stdscr.refresh()
         return None
-
-
-def display_key(key, stdscr):
-    try:
-        stdscr.addstr(1, 0, str(key))
-    except Exception as e:
-        print('Cannot display key')
-        print(e)
 
 
 wrapper(main)
